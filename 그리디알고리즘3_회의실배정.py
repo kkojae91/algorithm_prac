9385d14cd7fa4f9832bd5408c059dccaddea8efd
kojaejeung <jaejeung3210@likelion.org>
2020-08-06 02:43:00 +0900

    회의실배정 그리디알고리즘

---
+++ 그리디알고리즘3_회의실배정.py
@@ -0,0 +1,34 @@
+# 한 개의 회의실에서 회의
+# 회의시작하면 중단 불가능.
+# 회의의 시작시간과 끝나는 시간이 같을 수 있다.
+# 첫째 줄에 최대 사용할 수 있는 회의의 최대 개수를 출력한다.
+# n = 11
+# time_table = (1,4),(3,5),(0,6),(5,7),(3,8),(5,9),(6,10),(8,11),(8,12),(2,13),(12,14)
+# 기존입력값[(1, 4), (3, 5), (0, 6), (5, 7), (3, 8), (5, 9), (6, 10), (8, 11), (8, 12), (2, 13), (12, 14)]
+# 앞만정렬값[(0, 6), (1, 4), (2, 13), (3, 5), (3, 8), (5, 7), (5, 9), (6, 10), (8, 11), (8, 12), (12, 14)]
+# 뒤만정렬값[(1, 4), (3, 5), (0, 6), (5, 7), (3, 8), (5, 9), (6, 10), (8, 11), (8, 12), (2, 13), (12, 14)]
+# 앞뒤정렬값[(1, 4), (3, 5), (0, 6), (5, 7), (3, 8), (5, 9), (6, 10), (8, 11), (8, 12), (2, 13), (12, 14)]
+time_table = []
+n = int(input())
+for _ in range(n):
+    start, end = map(int, input().split())
+    time_table.append((start,end))
+    time_table = sorted(time_table, key=lambda time: time[0])
+    # print(time_table)
+    time_table = sorted(time_table, key=lambda time: time[1])
+    # print(time_table)
+print(time_table)
+
+def max_time_count(time_table):
+    time_idx = 0
+    time_count = 0
+    for time in time_table:
+        if time[0] >= time_idx:
+            time_idx = time[1]
+            time_count += 1
+    return time_count
+print(max_time_count(time_table))
+
+#####################
+# 재귀함수로
+##################
