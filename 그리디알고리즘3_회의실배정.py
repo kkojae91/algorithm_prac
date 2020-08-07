621bebb1510313c203578df5e84bd4a12e5a594a
kojaejeung <jaejeung3210@likelion.org>
2020-08-07 11:57:06 +0900

    회의실배정 강사님 코드 추가 및 정리

--- 그리디알고리즘3_회의실배정.py
+++ 그리디알고리즘3_회의실배정.py
@@ -8,27 +8,51 @@
 # 앞만정렬값[(0, 6), (1, 4), (2, 13), (3, 5), (3, 8), (5, 7), (5, 9), (6, 10), (8, 11), (8, 12), (12, 14)]
 # 뒤만정렬값[(1, 4), (3, 5), (0, 6), (5, 7), (3, 8), (5, 9), (6, 10), (8, 11), (8, 12), (2, 13), (12, 14)]
 # 앞뒤정렬값[(1, 4), (3, 5), (0, 6), (5, 7), (3, 8), (5, 9), (6, 10), (8, 11), (8, 12), (2, 13), (12, 14)]
-time_table = []
 n = int(input())
+time_table = []
 for _ in range(n):
     start, end = map(int, input().split())
-    time_table.append((start,end))
+    time_table.append([start,end])
     time_table = sorted(time_table, key=lambda time: time[0])
     # print(time_table)
     time_table = sorted(time_table, key=lambda time: time[1])
     # print(time_table)
 print(time_table)
 
+# time_table.sort(key=lambda time: [time[1],time[0]])
+# print(time_table)
+
 def max_time_count(time_table):
     time_idx = 0
     time_count = 0
+
     for time in time_table:
         if time[0] >= time_idx:
             time_idx = time[1]
+
             time_count += 1
+
     return time_count
 print(max_time_count(time_table))
 
-#####################
-# 재귀함수로
-##################
+###############
+# 강사님 코드
+# n = int(input())
+# meet_list = []
+
+# for _ in range(n):
+#     meet_list.append(list(map(int, input().split())))
+
+# meet_list.sort(key=lambda meet: [meet[1],meet[0]])
+# print(meet_list)
+
+# result_meet_cnt = 1
+# booked_end_time = meet_list[0][1]
+
+# for idx in range(1, len(meet_list)):
+#     if meet_list[idx][0] >= booked_end_time:
+#         booked_end_time = meet_list[idx][1]
+
+#         result_meet_cnt += 1
+
+# print(result_meet_cnt)
