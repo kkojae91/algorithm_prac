329bc6855fa05af8a86071af13103a59bb442571
kkojae <jaejeung3210@gmail.com>
2021-05-12 18:56:11 +0900

    programmers k번째 수

--- programmers/k번째수.py
+++ programmers/k번째수.py
@@ -1,42 +1,33 @@
-# 배열 array의 i번째 숫자부터 j번째 숫자까지 자르고 정렬했을 때, k번째에 있는 수를 구하려 합니다.
-# 예를 들어 array가 [1,5,2,6,3,7,4], i=2, j=5, k=3 이라면
+# 알파벳 소문자로 이루어진 N개의 단어가 들어오면 아래와 같은 조건에 따라 정렬하는 프로그램을 작성하시오.
+# 1. 길이가 짧은 것 부터
+# 2. 길이가 같으면 사전 순으로
 
-# 1. array의 2번째부터 5번째까지 자르면 [5,2,6,3]입니다.
-# 2. 1에서 나온 배열을 정렬하면 [2,3,5,6]입니다.
-# 3. 2에서 나온 배열의 3번째 숫자는 5입니다.
+# 첫째 줄에 단어의 개수 N이 주어진다. (1 <= N <= 20,000) 
+# 둘째 줄부터 N개의 줄에 걸쳐 알파벳 소문자로 이루어진 단어가 한 줄에 하나씩 주어진다. 
+# 주어지는 문자열의 길이는 50을 넘지 않는다.
 
-# 배열 array, [i,j,k]를 원소로 가진 2차원 배열 commands가 매개변수로 주어질 때, 
-# commands의 모든 원소에 대해 앞서 설명한 연산을 적용했을 때 나온 결과를 
-# 배열에 담아 return 하도록 solution 함수를 작성해주세요.
+# 조건에 따라 정렬하여 단어들을 출력한다. 단, 같은 단어가 여러 번 입력된 경우는 한 번씩만 출력한다.
 
-# 제한사항
-# array의 길이는 1이상 100 이하입니다.
-# array의 각 원소는 1이상 100 이하입니다.
-# commands의 길이는 1이상 50 이하입니다.
-# commands의 각 원소는 길이가 3 입니다.
-
-# 입출력 예
-# array = [1,5,2,6,3,7,4]
-# commands = [[2,5,3],[4,4,1],[1,7,3]]
-# return = [5,6,3]
-
-# 입출력 예 설명
-# [1,5,2,6,3,7,4]를 2번째부터 5번째까지 자른 후 정렬합니다. [2,3,5,6]의 세 번째 숫자는 5입니다.
-# [1,5,2,6,3,7,4]를 4번째부터 4번째까지 자른 후 정렬합니다. [6]의 첫 번째 숫자는 6입니다.
-# [1,5,2,6,3,7,4]를 1번째부터 7번째까지 자릅니다. [1,2,3,4,5,6,7]의 세 번째 숫자는 3입니다.
-
-def solution(array, commands):
-    answer = []
-    for command in commands:
-        command_list = array[command[0]-1:command[1]]
-        command_list.sort()
-        answer.append(command_list[command[2]-1])
-    return answer
+# 문자열 정렬
+def word_sort(word_list):
+    word_list.sort()
+    word_list.sort(key=lambda x: len(x))
+    
+    for word in word_list:
+        print(word)
 
 def init():
-    array = [1,5,2,6,3,7,4]
-    commands = [[2,5,3],[4,4,1],[1,7,3]]
+    N = int(input())
+    word_list=[]
 
-    print(solution(array, commands))
+    for i in range(N):
+        word = input()
+        if word in word_list:
+            pass
+        else:
+            word_list.append(word)
 
-init()    
+    
+    word_sort(word_list)
+
+init()
