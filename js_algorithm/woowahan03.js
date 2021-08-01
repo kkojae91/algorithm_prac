43fde3c3ee437db870e5d3a5e4e7fa3a674bd2b2
kkojae <jaejeung3210@gmail.com>
2021-08-01 13:30:51 +0900

    js 우아한03 + 예외처리

--- js_algorithm/woowahan03.js
+++ js_algorithm/woowahan03.js
@@ -1,6 +1,7 @@
 /*
 어느 연못에 청개구리가 살고 있고, 무슨 말이든 반대로 한다.
 엄마 말씀 word가 매개변수로 주어질 때, 아래 청개구리 사전을 참고하여 반대로 변환 후 return하는 프로그램 작성.
+예외는 공백, 특수문자일 경우!
 */
 
 function solution(words) {
@@ -35,8 +36,9 @@ function solution(words) {
   let answer = "";
   for (const word of words) {
     const upperWord = word.toUpperCase();
-    if (word === " ") {
-      answer += " ";
+    // 공백이거나, 특수문자일 경우
+    if (wordsHash[upperWord] === undefined) {
+      answer += word;
     } else if (word === word.toUpperCase()) {
       answer += wordsHash[word];
     } else {
@@ -47,7 +49,7 @@ function solution(words) {
 }
 
 function init() {
-  const word = "I love you";
+  const word = "I love you@#$$$";
   console.log(solution(word));
 }
 
