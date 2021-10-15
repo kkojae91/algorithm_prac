2a836d86f7ca2132698ac6a55c4bc87d7f5c00af
kkojae91 <jaejeung3210@gmail.com>
2021-10-15 22:10:26 +0900

    조합 구하기 다시

--- js_algorithm/inflearn/8_재귀함수와_완전탐색/조합구하기.js
+++ js_algorithm/inflearn/8_재귀함수와_완전탐색/조합구하기.js
@@ -1,5 +1,20 @@
 function solution(n, m) {
   let answer = [];
+  let temp = Array(m).fill(0);
+  function dfs(level, s) {
+    if (level === m) {
+      answer.push([...temp]);
+      return;
+    }
+
+    for (let i = s; i <= n; i++) {
+      temp[level] = i;
+      dfs(level + 1, i + 1);
+    }
+  }
+
+  dfs(0, 1);
+
   return answer;
 }
 
